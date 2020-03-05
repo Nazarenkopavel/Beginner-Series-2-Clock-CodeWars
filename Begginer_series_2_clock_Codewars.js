4504cca2f56cdbf10a1bff081c89593fc5557734
Nazarenkopavel <50582798+Nazarenkopavel@users.noreply.github.com>
2020-03-05 20:31:44 +0200

    Create Begginer_series_2_clock_Codewars.js

---
+++ Begginer_series_2_clock_Codewars.js
@@ -0,0 +1,14 @@
+/*
+Clock shows 'h' hours, 'm' minutes and 's' seconds after midnight.
+
+Your task is to make 'Past' function which returns time converted to miliseconds.
+*/
+
+function past(h, m, s) {
+  return h * 3600000 + m * 60000 + s * 1000;
+}
+
+console.log(past(0, 1, 1));
+console.log(past(1, 1, 1));
+console.log(past(1, 0, 0));
+console.log(past(1, 0, 1));
